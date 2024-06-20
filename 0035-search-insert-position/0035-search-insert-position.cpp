31016f3f8623c91a4c54ffe4f43fdd47e203a3aa
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2024-06-21 00:41:12 +0530

    Time: 0 ms (100%), Space: 12 MB (45.67%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,16 +4,20 @@ public:
         int l = 0;
         int r = nums.size() - 1;
         int mid;
+        int ans = nums.size();
         while (l <= r) {
             mid = l + (r - l) / 2;
  
-            if (nums[mid] == target)
-                return mid;
-            if (nums[mid] < target)
-                l = mid + 1;
-            else
+            if(nums[mid] >= target)
+            {
+                ans = mid;
                 r = mid - 1;
+            }
+            else
+            {
+                l = mid + 1;
+            }
         }
-        return l;
+        return ans;
     }
 };
