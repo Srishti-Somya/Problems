b92559b28ed9cc69cc0ba1425b5c4aa4b66b1ad6
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2024-06-22 09:17:37 +0530

    Time: 5 ms (62.89%), Space: 16 MB (49.56%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -43,18 +43,81 @@ public:
         return ans;
     }
 
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
-        int ub;
-        int lb = lower(nums, target);
-        if(lb == n || nums[lb] != target )
+    int first(vector<int>& nums, int target)
+    {
+        int low = 0;
+        int high = nums.size() - 1;
+        int mid;
+        int ans = -1;
+        while( low <= high)
         {
-            return {-1, -1};
+            mid = low + ( high - low ) / 2;
+            if(nums[mid] == target)
+            {
+                ans = mid;
+                high = mid - 1;
+            }
+            else if(nums[mid] < target)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    int last(vector<int>& nums, int target)
+    {
+        int low = 0;
+        int high = nums.size() - 1;
+        int mid;
+        int ans = -1;
+        while( low <= high)
+        {
+            mid = low + ( high - low ) / 2;
+            if(nums[mid] == target)
+            {
+                ans = mid;
+                low = mid + 1;
+            }
+            else if( nums[mid] > target)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        // int n = nums.size();
+        // int ub;
+        // int lb = lower(nums, target);
+        // if(lb == n || nums[lb] != target )
+        // {
+        //     return {-1, -1};
+        // }
+        // else
+        // {
+        //     ub = upper(nums, target);
+        // }
+        // return {lb, ub - 1};
+        int n = nums.size();
+        int firs = first(nums, target);
+        int las;
+        if(firs == n  )
+        {
+            return {-1,-1};
         }
         else
         {
-            ub = upper(nums, target);
+            las = last(nums, target);
         }
-        return {lb, ub - 1};
+        return {firs, las};
     }
 };
