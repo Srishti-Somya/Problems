dc27be68af74ff6acdc70209d93966120e0dcb2a
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2024-07-06 18:46:43 +0530

    Time: 2 ms (54.14%), Space: 7 MB (71.18%) - LeetHub

---
+++ 2645-pass-the-pillow/2645-pass-the-pillow.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        bool last = false;
+        int count = 1;
+        while(time)
+        {
+            if(count == n)
+            {
+                last = true;
+            }
+            if(count == 1)
+            {
+                last = false;
+            }
+            if(!last)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+            time--;
+        }
+        return count;
+    }
+};
