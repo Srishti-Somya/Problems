22415b474e312ce64a655d066691fe0426a7f84d
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2024-08-01 20:35:50 +0100

    Time: 3 ms (96.22%), Space: 21.4 MB (14.09%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,53 +1,18 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        vector<int>remove;
-        int distinct = 0;
-        for(int i = 0; i < nums.size();i++)
+        int n = nums.size();
+        int i = 0; 
+        int j = 1;
+        while( j < n)
         {
-            if(mp.find(nums[i])!= mp.end())
+            if( nums[i] != nums[j])
             {
-                remove.push_back(i);
-                continue;
-            } 
-            else
-            {
-                mp[nums[i]]++;
-                distinct++;
+                i++;
+                nums[i] = nums[j];
             }
-            // }
-            // if(nums[i-1] != nums[i])
-            // {
-            //     distinct++;
-            // }
+            j++;
         }
-        for(int i = 0 ; i < remove.size() ; i++ )
-        {
-            nums.erase(nums.begin() + remove[i] - i);
-        }
-        // cout<<distinct;
-        return distinct;
-        
-        
-        // int n = nums.size();
-        
-        // int distinct = 0;
-        // for( int i = 0; i < nums.size() ; i++ )
-        // {
-        //     if(nums[i] == nums[i + 1])
-        //     {
-        //         int curr_last = nums.size() - 1;
-        //         swap(nums[i] , nums[curr_last]);
-        //         nums.pop_back();
-        //         sort(nums.begin(), nums.end());
-        //     }
-        //     else
-        //     {
-        //         distinct++;
-        //     }
-        // }
-        // return distinct;
+        return i+1;
     }
-
 };
