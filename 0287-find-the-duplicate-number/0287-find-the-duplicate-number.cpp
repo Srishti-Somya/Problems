8e1aac99f6d7aaa9f36f6a26639f9a55a72cd76e
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2024-08-01 19:55:42 +0100

    Time: 77 ms (65.73%), Space: 63.9 MB (60.6%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,47 +1,8 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        // sort(nums.begin() , nums.end());
-        // for(int i = 1; i < nums.size(); i++)
-        // {
-        //     if(nums[i] == nums[i-1])
-        //     {
-        //         return nums[i];
-        //     }
-        // }
-        // return -1;
-        //we try to solve using floyds cycle detection
-        // int slow = nums[0];
-        // int fast = nums[0];
-        // do{
-        //     slow = nums[slow];
-        //     fast = nums[nums[fast]];
-        // } while(slow!=fast);
-
-        // fast = nums[0];
-        // while(slow!=fast){
-        //     slow = nums[slow];
-        //     fast = nums[fast];
-        // }
-        // return slow;
-        // unordered_map<int , int> mp;
-        // for(int i = 0; i < nums.size() ; i++ )
-        // {
-        //     mp[nums[i]]++;
-        // }
-        // for(auto a : mp)
-        // {
-        //     if(a.second >= 2)
-        //     {
-        //         return a.first;
-        //     }
-        // }
-        // return -1;
-
-        // tortoise and hare
-        
-        int slow = nums[0];
-        int fast = nums[0];
+        int slow = nums[0],fast = nums[0];
+        int n = nums.size();
         do{
             slow = nums[slow];
             fast = nums[nums[fast]];
@@ -53,6 +14,6 @@ public:
             slow = nums[slow];
             fast = nums[fast];
         }
-        return fast;
+        return slow;
     }
 };
