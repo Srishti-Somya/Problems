72c3af4107928ddb86cddfd03da5f629222d25be
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2024-08-14 04:02:25 +0100

    Time: 31 ms (15.24%), Space: 77.5 MB (42.44%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,98 +11,50 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        // int carry = 0;
-        // int value = 0;
-        // if( l1 != nullptr)
-        //     {
-        //         value += l1->val;
-        //     }
-        //     if(l2 != nullptr)
-        //     {
-        //         value += l2->val;
-        //     }
-        //     value += carry;
-        //     carry = value / 10;
-        // if(value >= 10)
-        // {
-            
-        //     value = value % 10;
-        // }
-        
-        // ListNode* newnode = new ListNode(value);
-        // ListNode* temp = newnode;
-        // l1 = l1 -> next;
-        // l2 = l2 -> next;
-        // value = 0;
-        // while( l1 != nullptr || l2 != nullptr )
-        // {
-        //     if( l1 != nullptr)
-        //     {
-        //         value += l1->val;
-        //     }
-        //     if(l2 != nullptr)
-        //     {
-        //         value += l2->val;
-        //     }
-        //     value += carry;
-        //     carry = value / 10;
-        //     if(value >= 10)
-        //     {
-        //         value = value % 10;
-        //     }
-        
-        //     ListNode* newnode1 = new ListNode(value);
-        //     temp->next = newnode1;
-        //     temp = temp->next;
-        //     if(l1 != nullptr)l1 = l1->next;
-        //     if(l2 != nullptr)l2 = l2->next;
-        //     value = 0;
-        // }
-        // return newnode;
-        ListNode* new_list = nullptr;
-    
-    int sum = 0;
-
-    while(l1 != nullptr || l2 != nullptr)
-    {
-        ListNode* new_node = new ListNode;
-        
-        if (l1 != nullptr) {
-            sum += l1->val;
-            l1 = l1->next;
-        }
-        if (l2 != nullptr) {
-            sum += l2->val;
-            l2 = l2->next;
-        }
-
-        new_node->val = sum % 10;
-        sum = sum / 10;
-        new_node->next = nullptr;
-
-        if(new_list == nullptr)
+        if(l1 == nullptr && l2 == nullptr)
         {
-            new_list = new_node;
-        }
-        else
+            return NULL;
+        }else if(l1 == NULL)
         {
-            ListNode* temp = new_list;
-            while(temp->next != nullptr)
+            return l2;
+        }else if(l2 == NULL)
+        {
+            return l1;
+        }
+
+        int value = l1->val + l2->val;
+        int carry = value/10;
+
+        ListNode* newlist = new ListNode(value%10);
+        ListNode* temp = newlist;
+        l1 = l1->next;
+        l2 = l2 -> next;
+        value = 0;
+        while(l1 != NULL || l2 != NULL)
+        {   
+            if(l1!= nullptr)
             {
-                temp = temp->next;
+                value += l1->val;
+                l1 = l1 -> next;
             }
-            temp->next = new_node;
+            if(l2 != nullptr)
+            {
+                value += l2 -> val;
+                l2 = l2 -> next;
+            }
+            value += carry;
+            carry = value/10;
+            ListNode* newnode = new ListNode(value%10);
+            temp->next = newnode;
+            temp = temp -> next;
+            value = 0;
         }
-    }   
-    if (sum > 0) {
-        ListNode* final_carry = new ListNode(sum);
-        ListNode* temp = new_list;
-        while(temp->next != nullptr)
+        if(carry != 0)
         {
-            temp = temp->next;
+            ListNode* newnode = new ListNode(carry);
+            temp->next = newnode;
+            temp = temp -> next;
         }
-        temp->next = final_carry;
-    }
-    return new_list;
+        return newlist;
     }
 };
