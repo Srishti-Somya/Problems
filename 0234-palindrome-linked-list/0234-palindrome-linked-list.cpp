2b40e0a77b052c170360272d4912bc6feda320ea
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2024-08-15 08:25:00 +0100

    Time: 164 ms (66.23%), Space: 130.9 MB (15.59%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,62 +10,27 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        // if(head == NULL || head->next == NULL){
-        //     return true;
-        // }
-
-        // ListNode *pnode, *cnode, *nnode;
-        // cnode = head;
-        // pnode = nullptr;
-        // while(cnode!=nullptr)
-        // {
-        //     nnode = cnode->next;
-        //     cnode->next = pnode;
-
-        //     pnode = cnode;
-        //     cnode = nnode;
-
-        // }
-
-        // while(head!=nullptr && pnode!=nullptr)
-        // {
-        //     if(head->val!=pnode->val){
-        //         return false;
-        //     }
-        //     else
-        //     {
-        //         head = head->next;
-        //         pnode = pnode->next;
-        //     }
-            
-        // }
-        // return true;
-        ListNode *slow = head;
-        ListNode *fast = head;
-        ListNode *prev = nullptr;
-        while(fast != nullptr && fast->next != nullptr)
+    
+    bool palindrome(vector<int>& arr)
+    {
+        int n = arr.size();
+        for( int i = 0; i < n/2; i++ )
         {
-            fast = (fast->next)->next;
-            ListNode *nextNode = slow->next;
-            slow->next = prev;
-            prev = slow;
-            slow = nextNode;
-        }
-        if(fast!= nullptr)
-        {
-            slow = slow->next;
-        }
-        while(slow != nullptr)
-        {
-            if(prev->val != slow->val)
+            if(arr[i] != arr[n - i - 1])
             {
                 return false;
             }
-            prev = prev->next;
-            slow = slow->next;
         }
         return true;
-
+    }
+    bool isPalindrome(ListNode* head) {
+        vector<int> arr;
+        ListNode* temp = head;
+        while( temp != nullptr)
+        {
+            arr.push_back(temp->val);
+            temp = temp->next;
+        }
+        return palindrome(arr);
     }
 };
