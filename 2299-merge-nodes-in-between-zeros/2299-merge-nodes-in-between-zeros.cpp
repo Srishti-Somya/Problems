f4def7c68e1beaef88c6fe51a5bf26b5e1771d5b
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2024-08-17 02:59:28 +0100

    Time: 462 ms (26.42%), Space: 279.5 MB (6.76%) - LeetHub

--- 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.cpp
+++ 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.cpp
@@ -11,42 +11,26 @@
 class Solution {
 public:
     ListNode* mergeNodes(ListNode* head) {
-        head = head->next;
-        int sum = head->val;
-        ListNode *temp = head;
-
-        ListNode *ans = new ListNode;
-        ListNode *new_node = ans;
-
+        ListNode* newlist = new ListNode(-1);
+        ListNode* curr = newlist;
+        ListNode* temp = head->next;
+        int sum = 0;
         while(temp != nullptr)
         {
-            if(temp -> val == 0)
+            if(temp->val == 0)
             {
-                if(sum != 0)
-                {
-                    new_node -> val = sum;
-                    if(temp->next != nullptr)
-                    {
-                        ListNode *node = new ListNode;
-                        new_node -> next = node;
-                        new_node = new_node->next;
-                    }
-                    else
-                    {
-                        new_node -> next = nullptr; 
-                    }
-                }
+                ListNode* newnode = new ListNode(sum);
+                curr->next = newnode;
+                curr = curr->next;
                 sum = 0;
+                temp = temp->next;
             }
-            temp = temp -> next;
-
-            if(temp != nullptr)
+            else
             {
-                sum += temp -> val;
+                sum += temp->val;
+                temp = temp -> next;
             }
-            
         }
-        // new_node = nullptr;
-        return ans;
+        return newlist->next;
     }
 };
