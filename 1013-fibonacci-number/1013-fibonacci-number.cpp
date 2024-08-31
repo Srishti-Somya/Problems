f4b272ac299e3ea0cfe48bb8f6176f7c1a60e07f
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2024-08-31 14:03:18 +0100

    Time: 1290 ms (16.11%), Space: 208.3 MB (11.04%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int solve(vector<int> &dp , int &n)
+    {
+        if( n <= 1)
+        {
+            return n;
+        }
+        if(dp[n] != -1)
+        {
+            return dp[n];
+        }
+        return fib(n-1) + fib(n-2);
+    }
+
+    int fib(int n) {
+        if( n <= 1)
+        {
+            return n;
+        }
+        vector<int> dp(n+1, -1);
+        return solve(dp, n);
+    }
+};
