6662bb71dce1fd9a070d777f720bc33e6ddf121c
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2024-08-31 14:26:48 +0100

    Time: 2 ms (80.87%), Space: 7.2 MB (77.23%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -18,13 +18,15 @@ public:
         {
             return n;
         }
-        vector<int> dp(n+1, -1);
-        dp[0] = 0;
-        dp[1] = 1;
+        int a = 0;
+        int b = 1;
+        int c;
         for( int i = 2; i <= n ; i++ )
         {
-            dp[i] = dp[i-1] + dp[i-2];
+            c = a + b;
+            a = b;
+            b = c;
         }
-        return dp[n];
+        return c;
     }
 };
