1ffbd08c19e492857a2e228709665ebb610b11d9
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2024-10-07 23:27:02 +0530

    Time: 9 ms (37.14%), Space: 13.3 MB (41%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> ans;
+        vector<int> grey;
+        for (int i = 0; i < pow(2 , n); ++i) {  
+            grey.push_back(i ^ (i >> 1));
+        }
+        return grey;
+    }
+};
