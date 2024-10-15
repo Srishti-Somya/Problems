68793c2799d434d496e4095b06ae6a97f3102adc
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2024-10-15 18:19:26 +0530

    Time: 29 ms (82.32%), Space: 15.6 MB (45.68%) - LeetHub

---
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long count = 0;
+        int n = s.size();
+        int black = 0;
+        // int white = 0;
+        for( int i = 0; i < n ; i++ )
+        {
+            if(s[i] == '0')
+            {
+                count += black;
+            }
+            else
+            {
+                black++;
+            }
+        }
+        // //using bubble sort
+        // bool swapped = false;
+        // for(int i = 0; i < n - 1 ; i++ )
+        // {
+        //     for(int j = 0 ; j < n - i - 1; j++ )
+        //     {
+        //         if(s[j] == '1' && s[j+1] == '0')
+        //         {
+        //             swap(s[j+1], s[j]);
+        //             swapped = true;
+        //             count++;
+        //         }
+        //     }
+        //     if(!swapped)
+        //     {
+        //         break;
+        //     }
+
+        // }
+        return count;
+    }
+};
