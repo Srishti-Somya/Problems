7040b9efbbdbf093c9a9415347734d42ddd0e7c9
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2025-03-24 00:22:49 +0530

    Time: 0 ms (100%), Space: 8 MB (96.47%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s == goal)
+        {
+            return true;
+        }
+        if(s.size() != goal.size())
+        {
+            return false;
+        }
+        for( int i = 0; i < s.size() ; i++ )
+        {
+            rotate(s.begin(), s.begin() + 1, s.end());
+            if(s == goal)
+            {
+                return true;
+            }
+        }
+        return false;
+        // if(s.size() != goal.size())
+        // {
+        //     return false;
+        // }
+        // int temp;
+        // for(int i = 0; i < s.size() ; i++ )
+        // {
+        //     if(goal[0] == s[i])
+        //     {
+        //         temp = i;
+        //         break;
+        //     }
+        // }
+        // int temp2 = temp;
+        // int count;
+        // for(int i = 0 ; i < goal.size() ; i++)
+        // {
+        //     if(temp != s.size())
+        //     {
+        //         if(goal[i] != s[temp])
+        //         {
+        //             return false;
+        //         }
+        //     }
+        //     else
+        //     {
+        //         count = i;
+        //         break;
+        //     }
+        //     temp++;
+        // }
+
+        // for(int i = 0; i < temp2 ; i++ )
+        // {
+        //     if(s[i] != goal[count])
+        //     {
+        //         return false;
+        //     }
+        //     count++;
+        // }
+        // return true;
+    }
+};
