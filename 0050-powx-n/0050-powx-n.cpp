3bd3cf3eef97aec38909be102e675e8f88581e8c
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2025-03-28 23:28:05 +0530

    Time: 0 ms (100%), Space: 8.7 MB (44.75%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     double myPow(double x, int n) {
+        if( x == 0)
+        {
+            return double(0);
+        }
+        if(x == 1)
+        {
+            return 1;
+        }
+        if(n == 0)
+        {
+            return 1;
+        }
         double ans = 1;
         long bin = n;
         if(bin < 0)
