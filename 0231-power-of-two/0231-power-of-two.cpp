5d069c2523aa5c36adb47a7b32cba733f1436d4c
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2025-03-30 01:33:22 +0530

    Time: 0 ms (100%), Space: 8 MB (19.52%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+        
         if(n == 1)
         {
             return true;
@@ -9,14 +10,19 @@ public:
         {
             return false;
         }
-        while(n > 1)
-        {
-            if(n%2 != 0)
-            {
-                return false;
-            }
-            n = n/2;
-        }
-        return true;
+        return ((n & (n-1)) == 0);
+        // while(n > 1)
+        // {
+        //     if(n >> 1 )
+        // }
+        // while(n > 1)
+        // {
+        //     if(n%2 != 0)
+        //     {
+        //         return false;
+        //     }
+        //     n = n/2;
+        // }
+        // return true;
     }
 };
