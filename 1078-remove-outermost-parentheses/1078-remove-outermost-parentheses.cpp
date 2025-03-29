960bf11374f5b8b444997344edef12b044942788
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2025-03-29 19:54:37 +0530

    Time: 0 ms (100%), Space: 8.9 MB (54.78%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,53 +1,27 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        int counter = 0;
+        int open = 0;
         string ans = "";
-        for(int i = 0; i < s.size() ; i++ )
+        for( int i = 0; i < s.size() ; i++ )
         {
-            if(s[i] == '(')
+            if(open == 0 && s[i] == '(')
             {
-                if(counter != 0){
-                    ans.push_back(s[i]);
-                }
-                counter++;
-            }
-            else
+                open++;
+            }else if( s[i] == ')')
             {
-                counter--;
-                if(counter != 0)
-                {
+                open--;
+                if(open == 0){
+                    continue;
+                }else{
                     ans.push_back(s[i]);
-                }
+                }   
             }
-        } 
+            else{
+                open++;
+                ans.push_back(s[i]);
+            }
+        }
         return ans;
-        // stack<char>st;
-        // int open = 0;
-        // for(int i = 0; i < s.size() ; i++ )
-        // {
-        //     if(s[i] == '(')
-        //     {
-        //         if(open != 0)
-        //         {
-        //             st.push(s[i]);
-        //         }
-        //         open++;
-        //     }else{
-        //         open--;
-        //         if(open != 0)
-        //         {
-        //             st.push(s[i]);
-        //         }
-        //     }
-        // }
-        // string temp = "";
-        // while(!st.empty())
-        // {
-        //     temp.push_back(st.top());
-        //     st.pop();
-        // }
-        // reverse(temp.begin(), temp.end());
-        // return temp;
     }
 };
