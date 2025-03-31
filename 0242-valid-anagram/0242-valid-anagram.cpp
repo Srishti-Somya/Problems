51a9cdebca73b00526fe6754c0d52567701a5070
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2025-03-31 11:59:07 +0530

    Time: 3 ms (54.75%), Space: 9.8 MB (48.33%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,43 +1,47 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // if(s.size() != t.size())
-        // {
-        //     return false;
-        // }
-        // sort(s.begin(), s.end());
-        // sort(t.begin(), t.end());
-        // for(int i = 0; i < s.size(); i++)
-        // {
-        //     if(s[i] != t[i])
-        //     {
-        //         return false;
-        //     }
-        // }
-        // return true;
-        unordered_map<char, int>mp;
-        for(int i = 0 ; i < s.size() ; i++ )
+        if(s.size() != t.size())
         {
-            mp[s[i]]++;
+            return false;
         }
-        for(int i = 0 ; i < t.size() ; i++ )
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        for(int i = 0; i < s.size(); i++)
         {
-            if(mp.find(t[i]) == mp.end())
-            {
-                return false;
-            }
-            else
-            {
-                mp[t[i]]--;
-            }
-        }
-        for(auto itr = mp.begin() ; itr != mp.end() ; itr++)
-        {
-            if(itr->second != 0)
+            if(s[i] != t[i])
             {
                 return false;
             }
         }
         return true;
+
+
+
+
+        // unordered_map<char, int>mp;
+        // for(int i = 0 ; i < s.size() ; i++ )
+        // {
+        //     mp[s[i]]++;
+        // }
+        // for(int i = 0 ; i < t.size() ; i++ )
+        // {
+        //     if(mp.find(t[i]) == mp.end())
+        //     {
+        //         return false;
+        //     }
+        //     else
+        //     {
+        //         mp[t[i]]--;
+        //     }
+        // }
+        // for(auto itr = mp.begin() ; itr != mp.end() ; itr++)
+        // {
+        //     if(itr->second != 0)
+        //     {
+        //         return false;
+        //     }
+        // }
+        // return true;
     }
 };
