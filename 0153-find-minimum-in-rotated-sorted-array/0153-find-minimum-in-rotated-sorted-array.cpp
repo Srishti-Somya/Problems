3862a749f80697bb8db0967100551c260b108d26
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2025-04-18 23:21:15 +0530

    Time: 0 ms (100%), Space: 14.2 MB (12.35%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,90 +1,22 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int low = 0;
-        int high = nums.size() - 1;
+        int st = 0;
+        int end = nums.size() - 1;
+        int mini = min(nums[st], nums[end]);
         int mid;
-        int target = INT_MAX;
-
-        while( low <= high )
-        {
-            mid = low + ( high - low ) / 2;
-            // if(nums[mid] < target)
-            // {
-            //     target = nums[mid];
-            // }
-            if( nums[low] <= nums[mid])
-            {
-                target = min(target, nums[low]);
-                low = mid + 1;
+        while(st <= end){
+            mid = st + (end - st)/2;
+            if(nums[mid] < mini) mini = nums[mid];
+            if(nums[mid] < nums[end]){
+                mini = min(nums[mid], mini);
+                end = mid - 1;
             }
-            else
-            {
-                target = min( target, nums[mid]);
-                high = mid - 1;
+            else{
+                mini = min(nums[st], mini);
+                st = mid + 1;
             }
         }
-        return target;
-
-        // int n = nums.size();
-        // int left = 0;
-        // int right = n-1;
-        // int mid;
-        
-        // while(left <= right)
-        // {
-        //     mid = left + (right-left)/2;
-        //     if(mid == n-1)
-        //     {
-        //         return nums[mid];
-        //     }
-        //     else if(mid == 0)
-        //     {
-        //         if(n == 2)
-        //         {
-        //             return min(nums[0],nums[1]);
-        //         }
-        //         return nums[0];
-        //     }
-        //     if(nums[mid] < nums[mid-1] && nums[mid] < nums[mid+1])
-        //     {
-        //         return nums[mid];
-        //     }
-        //     else
-        //     {
-        //         if(nums[mid-1]>nums[mid+1])
-        //         {
-        //             left = mid+1;
-        //         }
-        //         else
-        //         {
-        //             right = mid-1;
-        //         }
-        //     }
-        // }
-        // return nums[mid];
-        // int n = nums.size();
-		// int left = 0;
-		// int right = n - 1;
-		// int mid = 0;
-		// int target = INT_MAX;
-		// while (left <= right) {
-		// 	mid = (left + right) / 2;
-		// 	if (nums[mid] < target) {
-		// 		target = nums[mid];
-		// 	} else if (nums[left] < nums[mid]) {
-		// 		// left is sorted
-		// 		if(nums[mid] < nums[right])
-		// 			right = mid - 1;
-		// 		else
-		// 			left = mid + 1;
-		// 	} else {
-		// 		if(nums[mid] < nums[right])
-		// 			right = mid - 1;
-		// 		else
-		// 			left = mid + 1;
-		// 	}
-		// }
-		// return target;
+        return mini;
     }
 };
