37cf070a2258b8caa8e7ae1acf546d77370bb558
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2025-06-26 00:07:32 +0530

    Time: 3 ms (42.99%), Space: 21.1 MB (17.63%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,73 +1,33 @@
 class Solution {
 public:
-    void bfs(vector<vector<int>>& isConnected, vector<int> &visited, int n)
-    {
-        // vector<int> visited(n+1, 0);
-        queue<int> q;
-        q.push(n);
-        // visited[1] = 1;
-        while( !q.empty() )
-        {
-            int node = q.front();
-            q.pop();
-            // visited[node] = 1;
-            for(int i = 0; i < isConnected[node].size() ; i++)
-            {
-                if(isConnected[node][i] == 1)
-                {
-                    if(i == node)
-                    {
-                        visited[i] = 1;
-                    }
-                    if(!visited[i])
-                    {
-                        q.push(i);
-                        visited[i] = 1;
-                    }
-                }
+    void dfs(int start,vector<vector<int>>& adj, vector<int>& visited ){
+        visited[start] = 1;
+        for(auto it : adj[start]){
+            if(!visited[it]){
+                dfs(it, adj, visited);
             }
         }
         return;
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<int> visited(n, 0);
-        int count = 0;
-        for( int i = 0; i < n ; i++ )
-        {
-            if(!visited[i])
-            {
-                count++;
-                bfs(isConnected, visited, i);
+        vector<vector<int>>adj(n);
+        for(int i = 0 ; i < n ; i++ ){
+            for( int j = 0 ; j < n ; j++ ){
+                if(i != j && isConnected[i][j] == 1){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
             }
-            else
-            {
-                continue;
+        }
+        int count = 0;
+        vector<int>visited(n, 0);
+        for(int i = 0 ; i < n ; i++ ){
+            if(visited[i] == 0){
+                count++;
+                dfs(i, adj, visited);
             }
         }
         return count;
-        // visited[1] = 1;
-        // while(!q.empty())
-        // {
-            // int node = q.top();
-            // q.pop();
-            // for( int i = 1; i <= n ; i++ )
-            // {
-            //     for( int j = 1; j <= n ; j++ )
-            //     {
-            //         if(isConnected[i][j] == 1)
-            //         {
-            //             visited[i] = count;
-            //         }
-            //     }
-            // }
-            // for( int i = 1 ; i <= isConnected[node].size() ; i++ )
-            // {
-            //     if(isConnected[node][i] == 1)
-            //     {
-            //         visited[i] = 1;
-            //     }
-            // }
-        // }
     }
 };
