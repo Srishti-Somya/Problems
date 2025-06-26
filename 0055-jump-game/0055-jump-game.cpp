2f399299d17e1d3a673283e39a7d4fcb0477c5c5
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2025-06-26 09:26:42 +0530

    Time: 2 ms (31.1%), Space: 52.3 MB (63.94%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,12 +3,12 @@ public:
     bool canJump(vector<int>& nums) {
         int maxReachableIndex = 0;
         for(int i = 0 ; i < nums.size() ; i++ ){
+            if(i > maxReachableIndex) return false;
             if(i <= maxReachableIndex && nums[i]+i >= maxReachableIndex){
                 maxReachableIndex = nums[i]+i;
             }
-            if(i > maxReachableIndex) return false;
         }
-        if(maxReachableIndex+1 >= nums.size()) return true;
-        return false;
+        // if(maxReachableIndex+1 >= nums.size()) return true;
+        return true;
     }
 };
