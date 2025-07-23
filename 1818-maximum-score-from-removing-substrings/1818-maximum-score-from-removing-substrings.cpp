79352a381804792fac80e1128e88fcec0189cfc2
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2025-07-24 00:42:11 +0530

    Time: 58 ms (44.33%), Space: 28.9 MB (40.9%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -1,87 +1,106 @@
 class Solution {
 public:
     int maximumGain(string s, int x, int y) {
-        char start;
-        int score = 0;
-        if(x > y)
-        {
-            // start = 'a';
-        // }
-        // else
-        // {
-        //     start = 'b';
-        // }
+        if(s.size() == 1) return 0;
+        int maxi, mini;
+        string maximum, minimum;
+        if(x > y){
+            maxi = x;
+            mini = y;
+            maximum = "ab";
+            minimum = "ba";
+        }else{
+            maxi = y;
+            mini = x;
+            maximum = "ba";
+            minimum = "ab";
+        }
 
         stack<char>st;
-        for(int i = 0 ; i < s.size() ; i++ )
-        {
-            if(s[i] == 'b' && !st.empty() && st.top() == 'a')
-            {
-                score += x;
-                st.pop();
-            }
-            else
-            {
+        int res = 0;
+        st.push(s[0]);
+        int i = 1;
+
+        if(maximum == "ba"){
+            while(i < s.size()){
+                if(st.empty()) {st.push(s[i]); continue;}
+                if(s[i] == 'a' && st.top() == 'b'){
+                    res += maxi;
+                    st.pop();
+                    i++;
+                    continue;
+                }
                 st.push(s[i]);
+                i++;
             }
-        }
-        string temp = "";
-        while(!st.empty())
-        {
-            temp.push_back(st.top());
-            st.pop();
-        }
-        reverse(temp.begin(), temp.end());
-        for(int i = 0 ; i < temp.size() ; i++ )
-        {
-            if(temp[i] == 'a' && !st.empty() && st.top() == 'b')
-            {
-                score += y;
+            int j = 1;
+            string temp = "";
+            while(!st.empty()){
+                temp.push_back(st.top());
                 st.pop();
             }
-            else
-            {
-                st.push(temp[i]);
+            reverse(temp.begin(), temp.end());
+            if(temp.size() != 0 )st.push(temp[0]);
+            while(j < temp.size()){
+                if(st.empty()) {st.push(temp[j]); continue;}
+                if(temp[j] == 'b' && st.top() == 'a'){
+                    res += mini;
+                    st.pop();
+                    j++;
+                    continue;
+                }
+                st.push(temp[j]);
+                j++;
             }
-        }
-        return score;
-        }
-        else
-        {
-            stack<char>st;
-        for(int i = 0 ; i < s.size() ; i++ )
-        {
-            if(s[i] == 'a' && !st.empty() && st.top() == 'b')
-            {
-                score += y;
-                st.pop();
-            }
-            else
-            {
+        }else{
+
+            while(i < s.size()){
+                if(st.empty()) {st.push(s[i]); continue;}
+                if(s[i] == 'b' && st.top() == 'a'){
+                    res += maxi;
+                    st.pop();
+                    i++;
+                    continue;
+                }
                 st.push(s[i]);
+                i++;
             }
-        }
-        string temp = "";
-        while(!st.empty())
-        {
-            temp.push_back(st.top());
-            st.pop();
-        }
-        reverse(temp.begin(), temp.end());
-        for(int i = 0 ; i < temp.size() ; i++ )
-        {
-            if(temp[i] == 'b' && !st.empty() && st.top() == 'a')
-            {
-                score += x;
+            int j = 1;
+            string temp = "";
+            while(!st.empty()){
+                temp.push_back(st.top());
                 st.pop();
             }
-            else
-            {
-                st.push(temp[i]);
+            reverse(temp.begin(), temp.end());
+            if(temp.size() != 0 )st.push(temp[0]);
+            while(j < temp.size()){
+                if(st.empty()) {st.push(temp[j]); continue;}
+                if(temp[j] == 'a' && st.top() == 'b'){
+                    res += mini;
+                    st.pop();
+                    j++;
+                    continue;
+                }
+                st.push(temp[j]);
+                j++;
             }
         }
-        return score;
-        }
-        return score;
+        return res;
+
+        // int res = 0;
+        // while(s.find(maximum) != string::npos || s.find(minimum) != string::npos){
+        //     if(s.find(maximum) != string::npos){
+        //         size_t pos = s.find(maximum);
+        //         s.replace(pos, 2, "");
+        //         res += maxi;
+        //         continue;
+        //     }
+        //     if(s.find(minimum) != string::npos){
+        //         size_t pos = s.find(minimum);
+        //         s.replace(pos, 2, "");
+        //         res += mini;
+        //     }
+        // }
+        // return res;
     }
 };
