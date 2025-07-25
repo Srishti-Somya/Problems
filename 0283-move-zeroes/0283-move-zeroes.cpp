73683a7362d674884d5e8a6a8f00504a451a6420
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2025-07-25 18:37:14 +0530

    Time: 0 ms (100%), Space: 24 MB (9.84%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        // using two pointer with swap
-        int i = 0;
-        while(i < nums.size() && nums[i] != 0)
-        {
-            i++;
-        }
-        for(int j = i+1 ; j < nums.size() ; j++ )
-        {
-            if(nums[j] != 0)
-            {
-                swap(nums[i],nums[j]);
-                i++;
+        int j = 0; 
+        for(int i = 0 ; i < nums.size() ; i++ ){
+            if(nums[i] != 0){
+                nums[j] = nums[i];
+                j++;
             }
         }
-
-        // int curr = 0;
-        // for(int i = 0; i < nums.size() ; i++ )
-        // {
-        //     if(nums[i] != 0)
-        //     {
-        //         nums[curr] = nums[i];
-        //         curr++;
-        //     }
-        // }
-        // for(int i = curr ; i < nums.size() ; i++ )
-        // {
-        //     nums[i] = 0;
-        // }
+        while(j < nums.size()){
+            nums[j] = 0;
+            j++;
+        }
     }
 };
