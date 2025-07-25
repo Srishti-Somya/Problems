857a5875a43435240709a522ef12dc78e13a7c82
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2025-07-25 18:43:41 +0530

    Time: 0 ms (100%), Space: 97.5 MB (7.36%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        //building intuition
-        //the minimum selling price day jaise hi update ho, hum usse hi sellday
-        //bana dein, and ab uske baad condition check karein
-        // if sellday ya current price agar bada hai toh update sellday, then 
-        // then calculate cost, and maintain maximum
+        // //building intuition
+        // //the minimum selling price day jaise hi update ho, hum usse hi sellday
+        // //bana dein, and ab uske baad condition check karein
+        // // if sellday ya current price agar bada hai toh update sellday, then 
+        // // then calculate cost, and maintain maximum
 
-        int bestBuy = prices[0];
-        int maxProfit = 0;
+        // int bestBuy = prices[0];
+        // int maxProfit = 0;
 
-        for( int i = 1 ; i < prices.size() ; i++ )
-        {
-            if(prices[i] > bestBuy)
-            {
-                maxProfit = max(maxProfit, prices[i] - bestBuy);
-            }
-            bestBuy = min(bestBuy, prices[i]);
+        // for( int i = 1 ; i < prices.size() ; i++ )
+        // {
+        //     if(prices[i] > bestBuy)
+        //     {
+        //         maxProfit = max(maxProfit, prices[i] - bestBuy);
+        //     }
+        //     bestBuy = min(bestBuy, prices[i]);
+        // }
+        // return maxProfit;
+
+        int maxprofit = 0, mini = INT_MAX;
+        for(int i = 0 ; i < prices.size() ; i++ ){
+            mini = min(mini, prices[i]);
+            maxprofit = max(maxprofit, prices[i] - mini);
         }
-        return maxProfit;
+        return maxprofit;
     }
 };
