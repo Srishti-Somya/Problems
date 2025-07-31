b3a04e38ab7e7840852daed25db00c65c2ce1ec2
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2025-08-01 00:30:57 +0530

    Time: 3 ms (91.63%), Space: 15.3 MB (8.2%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -18,27 +18,48 @@ class Solution {
 public:
     Node* copyRandomList(Node* head) {
         if(head == nullptr) return head;
-        Node* newhead = new Node(head->val);
-        Node* old_temp = head->next;
-        Node* new_temp = newhead;
         unordered_map<Node*, Node*>mp;
-        mp[head]=newhead;
-        while(old_temp != nullptr){
-            Node* newNode = new Node(old_temp->val);
-            mp[old_temp] = newNode;
-            new_temp->next = newNode;
-            new_temp = new_temp->next;
-            old_temp = old_temp->next;
-            
+        mp[NULL] = NULL;
+        Node* newhead = new Node(head->val);
+        Node* oldtemp = head->next, *newtemp = newhead;
+        mp[head] = newhead;
+        while(oldtemp){
+            Node* newnode = new Node(oldtemp->val);
+            mp[oldtemp] = newnode;
+            newtemp->next = newnode;
+            newtemp = newtemp->next;
+            oldtemp = oldtemp->next;
         }
-        old_temp = head;
-        new_temp = newhead;
-        while(old_temp != nullptr){
-            Node* temp = old_temp->random;
-            new_temp->random = mp[temp];
-            old_temp = old_temp->next;
-            new_temp = new_temp->next;
+        oldtemp = head, newtemp = newhead;
+        while(oldtemp){
+            newtemp->random = mp[oldtemp->random];
+            oldtemp = oldtemp->next;
+            newtemp = newtemp->next;
         }
         return newhead;
+
+        // if(head == nullptr) return head;
+        // Node* newhead = new Node(head->val);
+        // Node* old_temp = head->next;
+        // Node* new_temp = newhead;
+        // unordered_map<Node*, Node*>mp;
+        // mp[head]=newhead;
+        // while(old_temp != nullptr){
+        //     Node* newNode = new Node(old_temp->val);
+        //     mp[old_temp] = newNode;
+        //     new_temp->next = newNode;
+        //     new_temp = new_temp->next;
+        //     old_temp = old_temp->next;
+            
+        // }
+        // old_temp = head;
+        // new_temp = newhead;
+        // while(old_temp != nullptr){
+        //     Node* temp = old_temp->random;
+        //     new_temp->random = mp[temp];
+        //     old_temp = old_temp->next;
+        //     new_temp = new_temp->next;
+        // }
+        // return newhead;
     }
 };
