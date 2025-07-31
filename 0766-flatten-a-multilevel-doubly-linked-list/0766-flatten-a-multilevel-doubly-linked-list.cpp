5cf0653037c50c422af3fa76978ea27582b36f68
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2025-08-01 00:50:53 +0530

    Time: 3 ms (67.27%), Space: 11.6 MB (72.72%) - LeetHub

--- 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.cpp
+++ 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.cpp
@@ -12,27 +12,48 @@ public:
 class Solution {
 public:
     Node* flatten(Node* head) {
-        if(head == nullptr) return nullptr;
-        
+        if(head == nullptr) return head;
         Node* curr = head;
-        while(curr != nullptr){
-            Node* temp = nullptr;
-            if(curr->child != nullptr){
-                temp = curr->next;
+        while(curr){
+            if(curr->child != NULL){
+                Node* nextn = curr->next;
                 curr->next = flatten(curr->child);
                 curr->next->prev = curr;
                 curr->child = nullptr;
-            
-            while(curr->next != nullptr){
-                curr = curr->next;
-            }
-            if(temp != nullptr){
-                curr->next = temp;
-                temp->prev = curr;
-            }
+                while(curr->next){
+                    curr = curr->next;
+                }
+                if(nextn){
+                    curr->next = nextn;
+                    nextn->prev = curr;
+                }
             }
             curr = curr->next;
         }
         return head;
+
+
+        // if(head == nullptr) return nullptr;
+        
+        // Node* curr = head;
+        // while(curr != nullptr){
+        //     Node* temp = nullptr;
+        //     if(curr->child != nullptr){
+        //         temp = curr->next;
+        //         curr->next = flatten(curr->child);
+        //         curr->next->prev = curr;
+        //         curr->child = nullptr;
+            
+        //     while(curr->next != nullptr){
+        //         curr = curr->next;
+        //     }
+        //     if(temp != nullptr){
+        //         curr->next = temp;
+        //         temp->prev = curr;
+        //     }
+        //     }
+        //     curr = curr->next;
+        // }
+        // return head;
     }
 };
