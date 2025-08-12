e7df3b0c3a5906184b1ec2a64e348c1287315b2a
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2025-08-13 00:12:42 +0530

    Time: 41 ms (6.85%), Space: 108.8 MB (8.23%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,29 +1,57 @@
 class Solution {
 public:
-    int islandPerimeter(vector<vector<int>>& grid) {
-         int n = grid.size();
-        int m = grid[0].size();
-        int cnt = 0;
+    void bfs(vector<vector<int>>& grid, vector<vector<int>>& vis, int &sr, int &sc, int &peri){
+        int n = grid.size(), m = grid[0].size();
+        vis[sr][sc] = 1;
+        queue<pair<int, int>>q;
+        q.push({sr, sc});
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                
-                if (grid[i][j] == 1) { // Checking only for land areas
-                    
-                    // for upper block
-                    if (i == 0 || grid[i-1][j] == 0) cnt++;
-                    
-                    // for down block
-                    if (i == n-1 || grid[i+1][j] == 0) cnt++;
-                    
-                    // for left block
-                    if (j == 0 || grid[i][j-1] == 0) cnt++;
-                    
-                    // for right block
-                    if (j == m-1 || grid[i][j+1] == 0) cnt++;
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            int top = row - 1, bottom = row + 1;
+            int right = col + 1, left = col - 1;
+
+            if(top < 0 || grid[top][col] == 0 )peri++;
+            if(bottom >= n  || grid[bottom][col] == 0 )peri++;
+            if(left < 0 || grid[row][left] == 0 )peri++;
+            if(right >= m || grid[row][right] == 0) peri++;
+
+            if(top >= 0 && grid[top][col] == 1 && vis[top][col] != 1){
+                q.push({top,col});
+                vis[top][col] = 1;
+            }
+            if(bottom < n && grid[bottom][col] == 1 && vis[bottom][col] != 1){
+                q.push({bottom,col});
+                vis[bottom][col] = 1;
+            }
+            if(left >= 0 && grid[row][left] == 1 && vis[row][left] != 1){
+                q.push({row,left});
+                vis[row][left] = 1;
+            }
+            if(right < m && grid[row][right] == 1 && vis[row][right] != 1){
+                q.push({row,right});
+                vis[row][right] = 1;
+            }
+        }
+    }
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>>vis(n, vector<int>(m, 0));
+        int sr, sc;
+        for(int i = 0 ; i < grid.size() ; i++ ){
+            for(int j = 0 ; j < grid[0].size() ; j++ ){
+                if(grid[i][j] == 1){
+                    sr = i;
+                    sc = j;
+                    break;
                 }
             }
         }
-        return cnt;
+        int ans = 0;
+        bfs(grid, vis, sr, sc, ans);
+        return ans;
     }
 };
