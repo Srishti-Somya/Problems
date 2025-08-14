ac733b1eace4dcb945a7626a94e6246ef4fbe71c
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2025-08-15 00:27:30 +0530

    Time: 43 ms (86.31%), Space: 28.9 MB (89.78%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
-        set<vector<int>>st;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>>ans;
         for(int i = 0 ; i < nums.size() ; i++ ){
-            unordered_map<int, int>mp;
-            for(int j = i+1 ; j < nums.size() ; j++ ){
-                int num = -(nums[i]+nums[j]);
-                if(mp.find(num) != mp.end()){
-                    vector<int>temp = {nums[i], nums[j], num};
-                    sort(temp.begin(), temp.end());
-                    st.insert(temp);
+            if(i != 0 && nums[i] == nums[i-1]) continue;
+            int j = i + 1;
+            int k = nums.size()-1;
+            while(j < k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum < 0){
+                    j++;// while(nums[j] == nums[j+1])j++;
+                }else if(sum > 0){
+                    k--;// while(nums[k] == nums[k-1])k--;
+                }else{
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                    while(j < k && nums[j] == nums[j-1])j++;
+                    while(j < k && nums[k] == nums[k+1])k--;
                 }
-                mp[nums[j]]++;
             }
         }
-        vector<vector<int>>ans(st.begin(), st.end());
         return ans;
     }
 };
