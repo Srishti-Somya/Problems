862b4d8c8bfae54624e2a43dad88d78934691f69
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2025-08-28 16:05:47 +0530

    Time: 0 ms (100%), Space: 12.4 MB (67.41%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -7,12 +7,23 @@ public:
         if(nums[0] > nums[1]) return 0;
         if(nums[n-1] > nums[n-2]) return n-1;
         while(st <= end){
-            int mid = st + (end-st)/2;
-            if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid+1]) return mid;
-
-            if(nums[mid] > nums[mid-1]) st = mid+1;
-            else end = mid - 1;
+            int mid = st + (end - st)/2 ;
+            if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid+1]){ return mid;}
+            else if(nums[mid-1] > nums[mid] ) end = mid - 1;
+            else st = mid + 1;
         }
         return -1;
+        
+
+
+
+        // while(st <= end){
+        //     int mid = st + (end-st)/2;
+        //     if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid+1]) return mid;
+
+        //     if(nums[mid] > nums[mid-1]) st = mid+1;
+        //     else end = mid - 1;
+        // }
+        // return -1;
     }
 };
