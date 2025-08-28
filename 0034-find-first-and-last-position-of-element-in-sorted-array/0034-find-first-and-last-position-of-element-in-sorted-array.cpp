b4697bafe08d5852eb0cce485ec17ca3e642b11b
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2025-08-28 23:07:49 +0530

    Time: 0 ms (100%), Space: 17.6 MB (17.54%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,33 +1,54 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int st = 0; 
-        int end = nums.size() - 1;
-        vector<int>ans(2,-1);
-        int first = end + 1, second = -1;
+        int first = -1, second = -1;
+        int st = 0, end = nums.size()-1;
         while(st <= end){
             int mid = st + (end - st)/2;
-            if(nums[mid] >= target){
-                first = min(first, mid);
+            if(nums[mid] == target){
+                first = mid;
                 end = mid - 1;
-            }
-            else{
-                st = mid + 1;
-            }
+            }else if(nums[mid] < target) st = mid + 1;
+            else end = mid - 1; 
         }
-        st = 0, end = nums.size() - 1;
+        st = 0, end = nums.size()-1;
         while(st <= end){
             int mid = st + (end-st)/2;
-            if(nums[mid] <= target){
-                second = max(second,mid);
+            if(nums[mid] == target) {
+                second = mid;
                 st = mid + 1;
-            }else{
-                end = mid - 1;
-            }
-        }
-        if(first == end+1){
-            return {-1,-1};
+            }else if(nums[mid] < target) st = mid + 1;
+            else end = mid - 1;
         }
         return {first, second};
+
+        // int st = 0; 
+        // int end = nums.size() - 1;
+        // vector<int>ans(2,-1);
+        // int first = end + 1, second = -1;
+        // while(st <= end){
+        //     int mid = st + (end - st)/2;
+        //     if(nums[mid] >= target){
+        //         first = min(first, mid);
+        //         end = mid - 1;
+        //     }
+        //     else{
+        //         st = mid + 1;
+        //     }
+        // }
+        // st = 0, end = nums.size() - 1;
+        // while(st <= end){
+        //     int mid = st + (end-st)/2;
+        //     if(nums[mid] <= target){
+        //         second = max(second,mid);
+        //         st = mid + 1;
+        //     }else{
+        //         end = mid - 1;
+        //     }
+        // }
+        // if(first == end+1){
+        //     return {-1,-1};
+        // }
+        // return {first, second};
     }
 };
