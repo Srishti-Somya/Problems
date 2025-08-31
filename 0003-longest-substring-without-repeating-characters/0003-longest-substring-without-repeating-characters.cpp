e32621c6f9500f8697d5b3466462a887ee5418ce
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2025-08-31 18:48:46 +0530

    Time: 15 ms (39.99%), Space: 14.3 MB (30.9%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,29 +1,49 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        int st = 0, end = 0, len = 0;
         unordered_map<char, int>mp;
-        int len = 0;
-        int start = 0;
-        int ans = 0;
-        int st = 0;
-        for(int i = 0; i < s.size() ; i++ ){
-            if(mp.find(s[i]) != mp.end()){
-                start = mp[s[i]];
-                len = i - start;
-                cout<< "len= " << len << " ";
-                while(st <= start){
+        while(end < n){
+            if(mp.find(s[end]) != mp.end()){
+                while(st <= mp[s[end]]){
                     mp.erase(s[st]);
                     st++;
-                } 
+                }
             }
-            else{
-                len++;
-                cout<< "len= " << len << " ";
-            }
-            mp[s[i]] = i;
-            ans = max(len, ans);
-            cout<< "ans= " << ans << endl;
+            mp[s[end]] = end;
+            len = max(len, end - st + 1);
+            end++;
         }
-        return ans;
+        return len;
+
+
+
+
+
+        // unordered_map<char, int>mp;
+        // int len = 0;
+        // int start = 0;
+        // int ans = 0;
+        // int st = 0;
+        // for(int i = 0; i < s.size() ; i++ ){
+        //     if(mp.find(s[i]) != mp.end()){
+        //         start = mp[s[i]];
+        //         len = i - start;
+        //         // cout<< "len= " << len << " ";
+        //         while(st <= start){
+        //             mp.erase(s[st]);
+        //             st++;
+        //         } 
+        //     }
+        //     else{
+        //         len++;
+        //         // cout<< "len= " << len << " ";
+        //     }
+        //     mp[s[i]] = i;
+        //     ans = max(len, ans);
+        //     // cout<< "ans= " << ans << endl;
+        // }
+        // return ans;
     }
 };
